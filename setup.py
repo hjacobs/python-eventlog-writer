a89950548c2f83cef7e4d2fb16dd5336f409b957
Tomasz Elendt <tomasz.elendt@zalando.de>
2013-06-03 14:26:35 +0000

    PF-677 switched from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='zalando-eventlog', version='0.1', py_modules=['eventlog'])
